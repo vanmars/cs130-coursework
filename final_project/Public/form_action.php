d56713ee79c82b813071a7a01a1540505e7d5cdf
Marie <Marie@Vanessas-MBP.hsd1.wa.comcast.net>
2020-06-02 15:20:12 -0700

    Updating changes to final project folder

--- final_project/Public/form_action.php
+++ final_project/Public/form_action.php
@@ -1,3 +1,42 @@
+<?php
+// define variables and set to empty values
+$yc1 = $y1e1 = $y1e2 = $y1cc = $y1rh =
+$yc2 = $y2e1 = $y2e2 = $y2cc = $y2rh =
+$pc2 = $p2e1 = $p2e2 = $p2cc = $p2rh = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $yc1 = test_input($_POST["yc1"]);
+  $y1e1 = test_input($_POST["y1e1"]);
+  $y1e2 = test_input($_POST["y1e2"]);
+  $y1cc = test_input($_POST["y1cc"]);
+  $y1rh = test_input($_POST["y1rh"]);
+
+  $yc2 = test_input($_POST["yc2"]);
+  $y2e1 = test_input($_POST["y2e1"]);
+  $y2e2 = test_input($_POST["y2e2"]);
+  $y2cc = test_input($_POST["y2cc"]);
+  $y2rh = test_input($_POST["y2rh"]);
+
+  $pc1 = test_input($_POST["pc1"]);
+  $p1e1 = test_input($_POST["p1e1"]);
+  $p1e2 = test_input($_POST["p1e2"]);
+  $p1cc = test_input($_POST["p1cc"]);
+  $p1rh = test_input($_POST["p1rh"]);
+
+  $pc1 = test_input($_POST["pc2"]);
+  $p1e1 = test_input($_POST["p2e1"]);
+  $p1e2 = test_input($_POST["p2e2"]);
+  $p1cc = test_input($_POST["p2cc"]);
+  $p1rh = test_input($_POST["p2rh"]);
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -7,6 +46,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="CSS/board.css"/>
     <link rel="stylesheet" href="https://use.typekit.net/zga1hzm.css">
+    <script type="text/javascript" src="JS/board.js"></script>
   </head>
 
 
@@ -22,10 +62,8 @@
                 <button onclick="document.location = 'islander_board.html'" id="view">Islander View</button>
             </div>
             <div id="icon-header">
-                <!-- <i class="fas fa-street-view" id="man"></i> -->
                 <p id="turn-text">YELLOW's Turn</p>
                 <img id="boat" src="../1x/asset1.png">
-                <!-- <i class="fas fa-ship" id="ship"></i> -->
             </div>
           </div>
     </header>
@@ -34,87 +72,85 @@
         <article id="board">
             <section class="info-card">
                   <div  class="yellow" id="card1">
-                      <p>“Maybe there is a beast… maybe it's only us.”</p>
+                      <p><?php echo $_POST["y1e1"]; ?></p>
                   </div>
             </section>
             <section class="info-card">
                   <div class="yellow" id="card2">
-                      <p>“Ralph wept for the end of innocence, the darkness of man's heart, and the fall through the air of the true, wise friend called Piggy.”</p>
+                      <p><?php echo $_POST["y1e2"]; ?></p>
                   </div>
             </section>
             <section class="info-card">
                   <div class="yellow" id="card3">
-                      <p>“The thing is - fear can't hurt you any more than a dream.”</p>
+                      <p><?php echo $_POST["y2e1"]; ?></p>
                   </div>
             </section>
             <section class="info-card">
                   <div class="yellow" id="card4">
-                      <p>“We did everything adults would do. What went wrong?”</p>
+                      <p><?php echo $_POST["y2e2"]; ?></p>
                   </div>
             </section>
             <section class="info-card">
                   <div class="purple" id="card5">
-                      <p>“The greatest ideas are the simplest.”</p>
+                      <p><?php echo $_POST["p1e1"]; ?></p>
                   </div>
             </section>
             <section class="info-card">
                   <div class="purple" id="card6">
-                      <p>“I think women are foolish to pretend they are equal to men, they are far superior and always have been.”</p>
+                      <p><?php echo $_POST["p1e2"]; ?></p>
                   </div>
             </section>
             <section class="info-card">
                   <div class="purple" id="card7">
-                      <p>“Fancy thinking the Beast was something you could hunt and kill! You knew, didn’t you? I’m part of you? Close, close, close! I’m the reason why it’s no go? Why things are what they are?”</p>
+                      <p><?php echo $_POST["p2e1"]; ?></p>
                   </div>
             </section>
             <section class="info-card">
                   <div class="purple" id="card8">
-                      <p>“What are we? Humans? Or animals? Or savages?”</p>
+                      <p><?php echo $_POST["p2e2"]; ?></p>
                   </div>
             </section>
             <section class="info-card">
                   <div class="red" id="card9">
-                      <p>“Sucks to your ass-mar!”</p>
+                      <p><?php echo $_POST["y1rh"]; ?></p>
                   </div>
             </section>
             <section class="info-card">
                   <div class="red" id="card10">
-                      <p>“If faces were different when lit from above or below -- what was a face? What was anything?”</p>
+                      <p><?php echo $_POST["y2rh"]; ?></p>
                   </div>
             </section>
             <section class="info-card">
                   <div class="red" id="card11">
-                      <p>“The world, that understandable and lawful world, was slipping away.”</p>
+                      <p><?php echo $_POST["p1rh"]; ?></p>
                   </div>
             </section>
             <section class="info-card">
                   <div class="red" id="card12">
-                      <p>“We've got to have rules and obey them. After all, we're not savages. We're English, and the English are best at everything.”</p>
+                      <p><?php echo $_POST["p2rh"]; ?></p>
                   </div>
             </section>
             <section class="info-card">
                   <div class="ccyellow" id="card13">
-                      <p>“He found himself understanding the wearisomeness of this life, where every path was an improvisation and a considerable part of one's waking life was spent watching one's feet.”</p>
+                      <p><?php echo $_POST["y1cc"]; ?></p>
                   </div>
             </section>
             <section class="info-card">
                   <div class="ccyellow" id="card14">
-                      <p>“They looked at each other, baffled, in love and hate.” </p>
+                      <p><?php echo $_POST["y2cc"]; ?></p>
                   </div>
             </section>
             <section class="info-card">
                   <div class="ccpurple" id="card15">
-                      <p>“The mask was a thing on it's own, behind which Jack hid, liberated from shame and self-conciousness.”</p>
+                      <p><?php echo $_POST["p1cc"]; ?></p>
                   </div>
             </section>
             <section class="info-card">
                   <div class="ccpurple" id="card16">
-                      <p>“His voice rose under the black smoke before the burning wreckage of the island; and infected by that emotion, the other little boys began to shake and sob too. And in the middle of them, with filthy body, matted hair, and unwiped nose, Ralph wept for the end of innocence, the darkness of mans heart, and the fall through the air of the true, wise friend called Piggy.”</p>
+                      <p><?php echo $_POST["p2cc"]; ?></p>
                   </div>
             </section>
         </article>
     </main>
-    <script src="https://kit.fontawesome.com/602bbba189.js" crossorigin="anonymous"></script>
-    <script type="text/javascript" src="JS/board.js"></script>
   </body>
 </html>
